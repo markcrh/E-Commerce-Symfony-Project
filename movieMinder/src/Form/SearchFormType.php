f72756c770a99017eba844bf83bd9e83bd464df2
Reboot Student <didivergente@gmail.com>
2024-11-14 12:40:58 +0000

    adsf

---
+++ movieMinder/src/Form/SearchFormType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class SearchFormType extends AbstractType
+{
+public function buildForm(FormBuilderInterface $builder, array $options)
+{
+$builder
+->add('query', TextType::class, [
+'label' => 'Search',
+'required' => false,
+'attr' => ['placeholder' => 'Search for products...']
+]);
+}
+
+public function configureOptions(OptionsResolver $resolver)
+{
+$resolver->setDefaults([]);
+}
+}
